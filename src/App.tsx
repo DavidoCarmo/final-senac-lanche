9d7e22708a8318c302ce53e7b8767b4c639c4737
Davi do Carmo <davigcarmo02@gmail.com>
2025-04-23 20:53:28 -0300

    ultimo-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React, { useReducer } from 'react';
-import { Box, Button, Container, Flex, Grid, Image, Stack, Text } from '@mantine/core';
+import { useEffect, useReducer, useState } from 'react';
+import { Button, Flex, Grid, Image, Stack, Title } from '@mantine/core';
+import { api } from './services/api';
 
 interface Lanche {
 
@@ -20,14 +21,15 @@ type Action =
   | { type: 'LIMPAR_PEDIDO' };
 
 
-const Lanches: Lanche[] = [
+// const Lanches: Lanche[] = [
 
-  { id: 1, nome: 'Hambúrguer', preco: 15.00, imagem: "../public/burguer.png" },
-  { id: 2, nome: 'Batata Frita', preco: 7.50, imagem: "../public/batata.png" },
-  { id: 3, nome: 'Cachorro Quente', preco: 10.00, imagem: "../public/cachorro.png" },
-  { id: 4, nome: 'Refrigerante', preco: 5.00, imagem: "../public/refri.png" },
-  { id: 5, nome: 'Sorvete', preco: 8.00, imagem: "../public/sorvete.png" }
-];
+//   { id: 1, nome: 'Hambúrguer', preco: 15.00, imagem: "../public/burguer.png" },
+//   { id: 2, nome: 'Batata Frita', preco: 7.50, imagem: "../public/batata.png" },
+//   { id: 3, nome: 'Cachorro Quente', preco: 10.00, imagem: "../public/cachorro.png" },
+//   { id: 4, nome: 'Refrigerante', preco: 5.00, imagem: "../public/refri.png" },
+//   { id: 5, nome: 'Sorvete', preco: 8.00, imagem: "../public/sorvete.png" }
+// ];
+let didInit = false;
 
 const initialState: State = { pedido: [], total: 0 };
 
@@ -64,6 +66,7 @@ const reducer = (state: State, action: Action): State => {
 
 
 export default function App() {
+  const [lanches, setLanches] = useState([] as Lanche[]);
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const adicionarItem = (item: Lanche) => {
@@ -83,43 +86,86 @@ export default function App() {
     }
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await api.get("/produtos", {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      console.log(data);
+      setLanches(data);
+    }
+    if (!didInit) {
+      didInit = true;
+      fetchData();
+    }
+  }, []);
+
   return (
-    <Flex direction={"column"} p={"xl"} align={"center"} bg="orange" justify={"center"}>
-      <Box w={"90%"} bg="blue">
-        <div id='minititulo'>
-          <img id='fotoFundo' src="../public/image.png" alt="" />
-          <h1 id='titulo'>Selecione seus Lanches</h1>
-        </div>
-      </Box>
+    <Flex direction={"column"} p={"xl"} align={"center"} justify={"center"}>
+
+      <Flex
+        direction={"column"}
+        justify={'center'}
+        align={'center'}
+        p={"xl"}
+        gap={"xl"}
+        w={"90%"}
+      >
+        <Image w={"30vw"} src="/senac-logo.png" alt="" />
+        <Title id='titulo'>Selecione seus Lanches</Title>
+      </Flex>
       <Stack w={"90%"}>
+        {lanches.map(item => (
 
-        {Lanches.map(item => (
-
-          <Grid key={item.id} w={"100%"} style={{
+          <Grid key={item.id} w={"100%"} p={"xl"} styles={{
             root: {
-              border: "1px black solid ",
+              border: "2px black solid ",
               borderRadius: "8px"
             }
-          }}>
-            <Grid.Col span={6} pl="lg"><Image radius="md" src={item.imagem} w={"30%"} /></Grid.Col>
-            <Grid.Col span={4}><Text span>{item.nome} - R${item.preco.toFixed(2)}</Text></Grid.Col>
-            <Grid.Col span={2}><Button onClick={() => adicionarItem(item)}>Adicionar</Button></Grid.Col>
+          }}
+          >
+            <Grid.Col span={2} pl="lg"><Image radius="md" src={item.imagem} w={"100%"} /></Grid.Col>
+            <Grid.Col span={8}><Title>{item.nome} - R${item.preco.toFixed(2)}</Title></Grid.Col>
+            <Grid.Col span={2}><Button size={"xl"} variant={"filled"} onClick={() => adicionarItem(item)}>Adicionar</Button></Grid.Col>
           </Grid>
 
         ))}
       </Stack>
-      <div className="pedido-lista">
-        <h2 id='pedido'>Pedido:</h2>
+      <Stack w={"90%"} >
+        <Title id='pedido'>Pedido:</Title>
         {state.pedido.map(item => (
-          <div key={item.id} className="pedido-item">
-            <img id='fotos' src={item.imagem} alt="" />
-            <span>{item.nome} - R${item.preco.toFixed(2)}</span>
-            <button onClick={() => removerItem(item.id)}>Remover</button>
-          </div>
+          <Grid key={item.id} w={"100%"} p={"xl"} styles={{
+            root: {
+              border: "2px black solid ",
+              borderRadius: "8px"
+            }
+          }}
+          >
+            <Grid.Col span={2} pl="lg"><Image radius="md" src={item.imagem} w={"100%"} /></Grid.Col>
+            <Grid.Col span={8}><Title>{item.nome} - R${item.preco.toFixed(2)}</Title></Grid.Col>
+            <Grid.Col span={2}><Button size={"xl"} variant={"filled"} onClick={() => removerItem(item.id)}>Remover</Button></Grid.Col>
+          </Grid>
         ))}
-      </div>
-      <h2>Total: R${state.total.toFixed(2)}</h2>
-      <button className="compra-button" onClick={handleCompraClick}>Comprar</button>
+        <Flex
+          direction={"column"}
+          justify={'center'}
+          align={'center'}
+          p={"xl"}
+          gap={"xl"}
+          w={"90%"}
+        >
+          <Title>Total: R${state.total.toFixed(2)}</Title>
+          <Button
+            size={"xl"}
+            variant={"filled"}
+            color={"green"}
+            onClick={handleCompraClick}
+          >Comprar</Button>
+        </Flex>
+
+      </Stack>
     </Flex>
   );
 };
